f2e609198b6cd43a20e4b907323eb9721b5ee61f
juanesgh <jerodriguez@miuandes.cl>
2020-06-12 00:24:34 -0400

    Linter(test): Adding spaces when needed, adding init files

--- test/test_block.py
+++ test/test_block.py
@@ -10,25 +10,25 @@ from block_test_cases_const import block1_value, block2_value, test_block1, test
 database = DBProvider()
 collection = database.select_collection("test")
 
-#Test Class Block methods
+# Test Class Block methods
 class TestBlocks(TestCase):
-    #Method Tested: to_json
-    #Context: Passing test model blocks
-    #Expectation: Should return OK
+    # Method Tested: to_json
+    # Context: Passing test model blocks
+    # Expectation: Should return OK
     def test_to_json(self):
         self.assertEqual(test_block1.to_json(),json_test )
 
-    #Method Tested: save_in_database
-    #Context: Passing test model blocks
-    #Expectation: Should return OK
+    # Method Tested: save_in_database
+    # Context: Passing test model blocks
+    # Expectation: Should return OK
     def test_save_in_database(self):
         test_block1.save_in_database(collection)
         block = collection.find_one({"x": str(test_block1.flyweight.block_data[1]),"y": str(test_block1.flyweight.block_data[2]),"z": str(test_block1.flyweight.block_data[3])})
         self.assertTrue(block, "the Block is save in database")
 
-    #Method Tested: print_block
-    #Context: Passing test model blocks
-    #Expectation: Should return OK
+    # Method Tested: print_block
+    # Context: Passing test model blocks
+    # Expectation: Should return OK
     def test_print_block(self):
         output = StringIO()          
         sys.stdout = output                   
@@ -36,42 +36,42 @@ class TestBlocks(TestCase):
         sys.stdout = sys.__stdout__                   
         self.assertEqual(output.getvalue(), "{'id': '0', 'x': '36', 'y': '286', 'z': '67', 'ton': '20.83', 'au': '0.2'}\n" )
 
-    #Method Tested: block_attribute
-    #Context: Passing test model blocks
-    #Expectation: Should return OK
+    # Method Tested: block_attribute
+    # Context: Passing test model blocks
+    # Expectation: Should return OK
     def test_block_attribute(self):
          json_without_attr = test_block2.block_attribute('z')
          self.assertEqual(json_without_attr, json_test_attr)
-    #Method Tested: block_mass
-    #Context: Passing test model blocks
-    #Expectation: Should return OK
+    # Method Tested: block_mass
+    # Context: Passing test model blocks
+    # Expectation: Should return OK
     def test_block_mass_case_ton(self):
-        self.assertEqual(test_block2.block_mass('ton','2'),0.08333)
+        self.assertEqual(test_block2.block_mass('ton', '2'),0.08333)
     def test_block_mass_case_kg(self):    
-        self.assertEqual(test_block2.block_mass('ton','1'),'83.33')
+        self.assertEqual(test_block2.block_mass('ton', '1'), '83.33')
     def test_block_mass_case_invalid_attr(self):    
-        self.assertEqual(test_block2.block_mass('tonn','1'), "The name of mass column was not valid (should be mass)")
+        self.assertEqual(test_block2.block_mass('tonn', '1'), "The name of mass column was not valid (should be mass)")
 
 
     
-    #Method Tested: block_grade
-    #Context: Passing test model blocks
-    #Expectation: Should return OK
+    # Method Tested: block_grade
+    # Context: Passing test model blocks
+    # Expectation: Should return OK
     def test_block_grade_case_tons_with_tons_mineral_type(self):
-        self.assertEqual(test_block2.block_grade('au','ton','2','2'),0.360014400576023)
+        self.assertEqual(test_block2.block_grade('au', 'ton', '2', '2'),0.360014400576023)
     def test_block_grade_case_kg_with_tons_mineral_type(self):
-        self.assertEqual(test_block2.block_grade('au','ton','1','2'),0.000360014400576023)
+        self.assertEqual(test_block2.block_grade('au', 'ton', '1', '2'),0.000360014400576023)
     def test_block_grade_case_invalid_attribute_mass(self):
-        self.assertEqual(test_block2.block_grade('au','tonn','2','2'),False)
+        self.assertEqual(test_block2.block_grade('au', 'tonn', '2', '2'),False)
     def test_block_grade_case_tons_and_percents_mineral_type(self):
-        self.assertEqual(test_block2.block_grade('au','ton','2','1'),test_block2.to_json()['au'])
+        self.assertEqual(test_block2.block_grade('au', 'ton', '2', '1'),test_block2.to_json()['au'])
     def test_block_grade_case_tons_with_kg_mineral_type(self):        
-        self.assertEqual(test_block2.block_grade('au','ton','2','3'), 360.01440057602304)
+        self.assertEqual(test_block2.block_grade('au', 'ton', '2', '3'), 360.01440057602304)
 
 
-    #Method Tested: get_keys_values
-    #Context: Passing test model blocks
-    #Expectation: Should return OK
+    # Method Tested: get_keys_values
+    # Context: Passing test model blocks
+    # Expectation: Should return OK
     def test_get_key_values(self):
         self.assertEqual(test_block1.get_key_values(), block_columns)
     
--- test/test_blockModel.py
+++ test/test_blockModel.py
@@ -31,62 +31,62 @@ test_rz = 10
 
 
 
-#Test Class BlockModel methods
+# Test Class BlockModel methods
 class TestBlockModels(TestCase):
-    #Method Tested: to_json
-    #Context: Passing test model blocks
-    #Expectation: Should return OK
+    # Method Tested: to_json
+    # Context: Passing test model blocks
+    # Expectation: Should return OK
     def test_blocks_count(self):
         self.assertEqual(test_block_model.blocks_count(collection),4 )
 
-    #Method Tested: save_in_database
-    #Context: Passing test model blocks
-    #Expectation: Should return OK
+    # Method Tested: save_in_database
+    # Context: Passing test model blocks
+    # Expectation: Should return OK
     def test_find_block_valid(self):
         block = collection.find_one({"x":'38', "y" : '28', "z" : '67'})
         test_block = Block(block.keys(), block.values())
-        self.assertEqual(test_block.to_json()["id"], test_block_model.find_block('38','28','67', collection).to_json()["id"] )
+        self.assertEqual(test_block.to_json()["id"], test_block_model.find_block('38', '28', '67', collection).to_json()["id"] )
     
     def test_find_block_invalid(self):
-        self.assertEqual(test_block_model.find_block('96','28','67', collection), False )
-    #Method Tested: max_coordinates
-    #Context: passing test model blocks
-    #should return ok
+        self.assertEqual(test_block_model.find_block('96', '28', '67', collection), False )
+    # Method Tested: max_coordinates
+    # Context: passing test model blocks
+    # should return ok
     def test_max_coordinates(self):
         new_coordinates = test_block_model.max_coordinates(collection)
-        self.assertEqual(new_coordinates, ['39', '28' ,'67'])
+        self.assertEqual(new_coordinates, ['39', '28' , '67'])
 
-    #Method Tested: continue_attributes
-    #Context: passing test model blocks
-    #should return ok
+    # Method Tested: continue_attributes
+    # Context: passing test model blocks
+    # should return ok
     def test_continue_attributes(self):
         attribute = "ton"
         bks = [test_block1.to_json(), test_block2.to_json()]
         new_value = test_block_model.continues_attributes(bks, attribute)
         self.assertEqual(new_value, 104.16)
 
-    #Method Tested: categorical_attributes
-    #Context: passing test model blocks
-    #should return ok
+    # Method Tested: categorical_attributes
+    # Context: passing test model blocks
+    # should return ok
     def test_categorical_attributes(self):
         attribute = "ton"
         bks = [test_block1.to_json(), test_block2.to_json()]
         new_value = test_block_model.categorical_attributes(bks, attribute)
         self.assertEqual(new_value, 83.33)
 
-    #Method Tested: proporcional_attributes
-    #Context: passing test model blocks
-    #should return ok
+    # Method Tested: proporcional_attributes
+    # Context: passing test model blocks
+    # should return ok
     def test_proportinal_attributes(self):
         attribute = "au"
         bks = [test_block1.to_json(), test_block2.to_json()]
         new_value = test_block_model.proportinal_attributes(bks, attribute, "ton")
         self.assertEqual(new_value, ((83.33*0.3 + 20.83*0.2)/104.16))
 
-    #Method Tested: reblock
-    #Context: passing test model blocks
-    #should return ok
-    #proving first and last block of a new model block
+    # Method Tested: reblock
+    # Context: passing test model blocks
+    # should return ok
+    # proving first and last block of a new model block
     def test_reblock(self):
         new_attribute = ["con", "prop"]
         new_collection = test_block_model.reblock(collection, test_rx, test_ry, test_rz, new_attribute, "ton")
@@ -94,10 +94,10 @@ class TestBlockModels(TestCase):
         last_block = new_collection.find_one({"x": 3, "y" : 2, "z" : 6}) 
         self.assertTrue(first_block, last_block)
     
-    #Method Tested: create_reblocked_block
-    #Context: passing test model blocks
-    #should return ok
-    #risize return a json of a new block with new parameters
+    # Method Tested: create_reblocked_block
+    # Context: passing test model blocks
+    # should return ok
+    # risize return a json of a new block with new parameters
     def test_create_reblocked_block(self):
         new_attribute = ["con", "prop"]
         test_resize = test_block_model.create_reblocked_block(collection,30,20,60,10,10,10,[3,2,6],83,new_attribute,"ton")
